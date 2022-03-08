92292ca962dde60c50bc93936b0f015656aff84d
ivopozzani <ivo.pozzani@gmail.com>
2022-03-08 10:11:53 -0300

    feat: added three rows of cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
 import "./App.css";
 import Card from "./components/Card";
+import initialCardsTrick from "../utils/initialCardsTrick";
+
+const cardSuits = ["spades", "clubs", "diamonds", "hearts"];
+const cardNumerals = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+const displayCards = initialCardsTrick(cardSuits, cardNumerals);
 
 function App() {
+  const row1 = displayCards.slice(0, 7);
+  const row2 = displayCards.slice(7, 14);
+  const row3 = displayCards.slice(14);
   return (
     <>
       <h1>Card Trick</h1>
       <div className="display-cards">
-        <Card />
+        <div>
+          <Card renderRow={row1} />
+        </div>
+        <div>
+          <Card renderRow={row2} />
+        </div>
+        <div>
+          <Card renderRow={row3} />
+        </div>
       </div>
     </>
   );
