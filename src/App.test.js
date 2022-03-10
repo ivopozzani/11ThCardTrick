21bbb01cb151f2dc345b4873f2b651604950e3f5
ivopozzani <ivo.pozzani@gmail.com>
2022-03-10 09:30:11 -0300

    feat: updated App component

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { render, screen } from "@testing-library/react";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Card Trick/i);
   expect(linkElement).toBeInTheDocument();
 });
