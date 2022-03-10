237683eeb4ce7fe9e05d1d0a9936ec071af418a5
ivopozzani <ivo.pozzani@gmail.com>
2022-03-10 10:46:52 -0300

    Feat: display chosen card to user

--- src/App.js
+++ src/App.js
@@ -20,18 +20,23 @@ const cardNumerals = [
   "K",
 ];
 const ROWSIZE = 7;
-const cardsToTrick = initialCardsTrick(cardSuits, cardNumerals);
-const firstRow = cardsToTrick.slice(0, ROWSIZE);
-const secondRow = cardsToTrick.slice(ROWSIZE, ROWSIZE * 2);
-const thirdRow = cardsToTrick.slice(ROWSIZE * 2);
+const setInitialState = () => {
+  const cardsToTrick = initialCardsTrick(cardSuits, cardNumerals);
+  const firstRow = cardsToTrick.slice(0, ROWSIZE);
+  const secondRow = cardsToTrick.slice(ROWSIZE, ROWSIZE * 2);
+  const thirdRow = cardsToTrick.slice(ROWSIZE * 2);
 
-function App({
-  initialState = { row1: firstRow, row2: secondRow, row3: thirdRow },
-}) {
-  const [rows, setRows] = useState(initialState);
+  return { row1: firstRow, row2: secondRow, row3: thirdRow };
+};
+let counter = 0;
+
+function App() {
+  const [rows, setRows] = useState(setInitialState());
+  const [activeState, setActiveState] = useState(false);
 
   const updateRowsState = (before, middle, after) => {
     const cardsToDeal = [...before, ...middle, ...after];
+    counter++;
 
     rows.row1 = [];
     rows.row2 = [];
@@ -43,47 +48,83 @@ function App({
       rows.row3.push(cardsToDeal.shift());
     }
     setRows({ row1: rows.row1, row2: rows.row2, row3: rows.row3 });
+
+    if (counter === 3) {
+      setActiveState(true);
+    }
+  };
+
+  const restartTrick = () => {
+    counter = 0;
+    setRows(setInitialState());
+    setActiveState(false);
   };
 
   return (
     <>
-      <h1 className="header">Card Trick</h1>
+      <h1 className={activeState ? "displayOpaque header" : "header"}>
+        Card Trick
+      </h1>
       <div className="display-cards">
-        <div className="row">
-          <div>
-            <button
-              onClick={() =>
-                updateRowsState([...rows.row2], [...rows.row1], [...rows.row3])
-              }
-            >
-              Row1
-            </button>
+        <div className={activeState ? "displayOpaque" : ""}>
+          <div className="row">
+            <div>
+              <button
+                disabled={activeState}
+                onClick={() =>
+                  updateRowsState(
+                    [...rows.row2],
+                    [...rows.row1],
+                    [...rows.row3]
+                  )
+                }
+              >
+                Row1
+              </button>
+            </div>
+            <Card renderRow={rows.row1} />
           </div>
-          <Card renderRow={rows.row1} />
+          <div className="row">
+            <div>
+              <button
+                disabled={activeState}
+                onClick={() =>
+                  updateRowsState(
+                    [...rows.row1],
+                    [...rows.row2],
+                    [...rows.row3]
+                  )
+                }
+              >
+                Row2
+              </button>
+            </div>
+            <Card renderRow={rows.row2} />
+          </div>
+          <div className="row">
+            <div>
+              <button
+                disabled={activeState}
+                onClick={() =>
+                  updateRowsState(
+                    [...rows.row1],
+                    [...rows.row3],
+                    [...rows.row2]
+                  )
+                }
+              >
+                Row3
+              </button>
+            </div>
+            <Card renderRow={rows.row3} />
+          </div>
         </div>
-        <div className="row">
+        <div className={activeState ? "displayBigger" : "displayNone"}>
+          <h2>Your Chosen Card is:</h2>
+          <Card renderRow={[rows.row2[3]]} />
           <div>
-            <button
-              onClick={() =>
-                updateRowsState([...rows.row1], [...rows.row2], [...rows.row3])
-              }
-            >
-              Row2
-            </button>
+            <button onClick={restartTrick}>Restart Trick</button>
           </div>
-          <Card renderRow={rows.row2} />
-        </div>
-        <div className="row">
-          <div>
-            <button
-              onClick={() =>
-                updateRowsState([...rows.row1], [...rows.row3], [...rows.row2])
-              }
-            >
-              Row3
-            </button>
-          </div>
-          <Card renderRow={rows.row3} />
         </div>
       </div>
     </>
